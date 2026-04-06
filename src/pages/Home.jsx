d306e042106b9ccafb26be11c90a0f969e398b15
Sandra Blanco <sandra.blanco18@gmail.com>
2026-04-06 13:17:04 +0200

    changes on Home.css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import '../Home.css'
 
 function Home() {
   const [client, setClient] = useState(null)
@@ -20,31 +21,34 @@ function Home() {
   }, [])
 
   return (
-    <div>
-      <h1>¡Escape Room Unfollow al corazón ❤️! 🔐</h1>
+    <div className="homeContainer">
+      <div className="homeCard">
+      <strong><h1>¡Unfollow al corazón ❤️! 🔐</h1></strong>
 
       {!client ? (
         <>
-          <p>En la era del 'visto' y las promesas vacías, el amor moderno se ha convertido en el enigma más difícil de resolver. ¿Cansado de recibir solo migajas o de desaparecer en el vacío del ghosting? Entra en nuestra sala, descifra las señales mezcladas del lovebombing y descubre si eres capaz de encontrar tu verdadero yo antes de que se agote el tiempo... o la batería de tu corazón😏</p>
-
+          <em><p className="text">En la era del 'visto' y las promesas vacías, el amor moderno se ha convertido en el enigma más difícil de resolver. ¿Cansado de recibir solo migajas o de desaparecer en el vacío del ghosting? Entra en nuestra sala, descifra las señales mezcladas del lovebombing y descubre si eres capaz de encontrar tu verdadero yo antes de que se agote el tiempo... o la batería de tu corazón😏</p></em>
+          <div className="buttonsContainer">
           <Link to="/login">
-            <button>Ya tengo cuenta, empezar ya!</button>
+            <button className="loginButton">Ya tengo cuenta, empezar ya!</button>
           </Link>
 
           <Link to="/register">
-            <button>No tengo cuenta</button>
+            <button className="registerButton">No tengo cuenta</button>
           </Link>
+          </div>
         </>
       ) : (
         <>
           <p>Bienvenido, {client.name} 👋</p>
 
           <Link to="/game">
-            <button>Continuemos con la aventura</button>
+            <button className="gameButton">Continua con la aventura</button>
           </Link>
         </>
       )}
     </div>
+  </div>
   )
 }
 
